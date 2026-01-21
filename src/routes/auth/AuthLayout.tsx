b5c8fe95c80cd6776fbe0c51e892bfdb1726cf7f
DeckofAces <52782439+DeckofAces@users.noreply.github.com>
2026-01-21 14:28:51 +0100

    auto navigate user to dashboard if logged in already

--- src/routes/auth/AuthLayout.tsx
+++ src/routes/auth/AuthLayout.tsx
@@ -5,8 +5,14 @@ import {
   type AnyRoute,
 } from "@tanstack/react-router";
 import fullLogo from "/assets/icons/full-logo.svg";
+import { useEffect } from "react";
+import { userToken } from "@/lib/utils";
 
 function AuthLayout() {
+  useEffect(() => {
+    if (userToken()) window.location.href = "/dashboard";
+  }, []);
+
   return (
     <main className="relative grid h-screen w-screen place-items-center bg-[#E7F2ED]">
       <Link to="/">
